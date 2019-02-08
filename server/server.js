7ac5f43ea6054e41e94a1cb3528ae657322ae3f9
mdvenkatesh <mdvenkatesh1421@gmail.com>
2019-02-08 19:04:01 +0530

    in these we have added test file

--- server/server.js
+++ server/server.js
@@ -12,31 +12,21 @@ var app = express();
 
 app.use(bodyParser.json());
 // post app
-// app.post('/todos', (req, res) => {
-//   var todo = new Todo({
-//     text:req.body.text
-//   });
-//   todo.save().then((doc) => {
-//     res.send(`here is the saved doc${doc}`)
-//   },(e) => {
-//     res.send(`here is the error which had occured${e}`)
-//   })
-// });
+
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text:req.body.text,
-    completed:true,
-    completedAt:44-56-78
   });
   todo.save().then((doc) => {
-    res.send(`here is the saved doc${doc}`)
+      res.send(doc);
   },(e) => {
-    res.send(`here is the error which had occured${e}`)
+      res.status(400).send(e);
   })
 });
-
 
 app.listen(3000,()=> {
   console.log("Started on the port");
 });
+
+module.exports = {app};
